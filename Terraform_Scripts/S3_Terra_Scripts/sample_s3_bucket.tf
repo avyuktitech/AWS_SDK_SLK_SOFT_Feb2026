5e8ddf088b2bc90768d596ae014588f98eea9bb8
avyuktitech <76204004+avyuktitech@users.noreply.github.com>
2026-03-03 11:04:44 +0530

    Uncomment S3 bucket resource configuration

--- Terraform_Scripts/S3_Terra_Scripts/sample_s3_bucket.tf
+++ Terraform_Scripts/S3_Terra_Scripts/sample_s3_bucket.tf
@@ -1,5 +1,5 @@
+ resource "aws_s3_bucket" "example" {
+   bucket = "my-unique-sample-bucket-20260220"
+   acl    = "private"
+ }
 
-# resource "aws_s3_bucket" "example" {
-#   bucket = "my-unique-sample-bucket-20260220"
-#   acl    = "private"
-# }
