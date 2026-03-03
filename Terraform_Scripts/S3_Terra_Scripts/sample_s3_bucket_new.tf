6583df637299b64c553285ce00bf08b3175dd759
avyuktitech <76204004+avyuktitech@users.noreply.github.com>
2026-03-03 11:05:03 +0530

    Uncomment S3 bucket resource configuration

--- Terraform_Scripts/S3_Terra_Scripts/sample_s3_bucket_new.tf
+++ Terraform_Scripts/S3_Terra_Scripts/sample_s3_bucket_new.tf
@@ -1,4 +1,5 @@
-#resource "aws_s3_bucket" "sample_bucket" {
-#  bucket = "my-sample-bucket-20260220-unique"
-#  force_destroy = true
-#}
+resource "aws_s3_bucket" "sample_bucket" {
+  bucket = "my-sample-bucket-20260220-unique"
+  force_destroy = true
+}
+
